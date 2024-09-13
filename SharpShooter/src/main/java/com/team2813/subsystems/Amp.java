536777e34a7347794863fdc49745cd6485ed8370
cuttestkittensrule <mangoiscute95@gmail.com>
2024-09-13 16:46:53 -0700

    Create Amp subsystem (#11)
    * Create Amp.java
    
    added code for the Amp subsystem. (#3 )
    
    * Correct CAN Id
    
    * Make indenting consistent
    
    ---------
    
    Co-authored-by: atlanticbomber <151278181+atlanticbomber@users.noreply.github.com>

---
+++ SharpShooter/src/main/java/com/team2813/subsystems/Amp.java
@@ -0,0 +1,32 @@
+package com.team2813.subsystems;
+
+import com.team2813.lib2813.control.ControlMode;
+import com.team2813.lib2813.control.InvertType;
+import com.team2813.lib2813.control.Motor;
+import com.team2813.lib2813.control.motors.TalonFXWrapper;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static com.team2813.Constants.AMP;
+
+public class Amp extends SubsystemBase {
+	private final Motor gary;
+	public Amp() {
+		gary = new TalonFXWrapper(AMP, InvertType.CLOCKWISE);
+	}
+
+	public void pushIn() {
+		gary.set(ControlMode.DUTY_CYCLE, 0.5);
+	}
+
+	public void pushOut() {
+		gary.set(ControlMode.DUTY_CYCLE, -0.5);
+	}
+
+	public void shootAmp() {
+		gary.set(ControlMode.DUTY_CYCLE, -0.5);
+	}
+
+	public void stop() {
+		gary.set(ControlMode.DUTY_CYCLE, 0);
+	}
+}
