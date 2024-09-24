3311a269504684a1c7ab5060f60ed0ca2bd8c35c
cuttestkittensrule <mangoiscute95@gmail.com>
2024-09-23 20:39:05 -0700

    Add various changes to make sysid work
    * Change elevator control to voltage
      * Sysid uses voltage; need to use voltage for control
    * Add positions for bottom and the top
      * We don't actually need this for sysid; but this will make our lives
        easier when we try to write positioning code
    * Update lib2813 to a version that supports voltage control
      * This part should be self-explanitory due to need of voltage control

--- SharpShooter/src/main/java/com/team2813/subsystems/Elevator.java
+++ SharpShooter/src/main/java/com/team2813/subsystems/Elevator.java
@@ -1,15 +1,13 @@
 package com.team2813.subsystems;
 
-import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
+import com.team2813.lib2813.control.ControlMode;
 import com.team2813.lib2813.control.InvertType;
 import com.team2813.lib2813.control.motors.TalonFXWrapper;
 import com.team2813.lib2813.subsystems.MotorSubsystem;
-
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Measure;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.Supplier;
 
 import static com.team2813.Constants.ELEVATOR_1;
 import static com.team2813.Constants.ELEVATOR_2;
@@ -19,12 +17,16 @@ public class Elevator extends MotorSubsystem<Elevator.Position> {
     
      public Elevator() {
         super(
-            new MotorSubsystemConfiguration(new TalonFXWrapper(ELEVATOR_1,InvertType.CLOCKWISE)));
-            ((TalonFXWrapper) motor).addFollower(ELEVATOR_2,InvertType.OPPOSE_MASTER);
+            new MotorSubsystemConfiguration(new TalonFXWrapper(ELEVATOR_1,InvertType.CLOCKWISE))
+                    .controlMode(ControlMode.VOLTAGE)
+        );
+        ((TalonFXWrapper) motor).addFollower(ELEVATOR_2,InvertType.OPPOSE_MASTER);
     }
 
     public enum Position implements Supplier<Measure<Angle>> {
-        TEST(0);
+        TEST(0),
+        BOTTOM(0),
+        TOP(0);
 
         private final Measure<Angle> position;
         Position(double position) {
